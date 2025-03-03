c81b597d57b6b7b24878c9a374a5454a49a06c44
Tobias Zimmermann <tobizimmermann@tsn.at>
2025-03-03 14:44:15 +0100

    I added a Main which creates a car an gives out the intel on the console
    Change-Id: Ifa5f8457b5b22cd69380ccbbf1cd37c4d774ceaf

--- src/Auto.java
+++ src/Auto.java
@@ -1,4 +1,3 @@
-
 public class Auto {
     private String make;
     private String model;
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Auto car = new Auto("Toyota", "Camry", 2021, 25000.00);
+		System.out.println(car.toString());
+	}
+
+}
