54ca91d08104a1bc2e50196fc79c40ef3c651a14
joseppe28 <josef0703.messner@gmail.com>
2025-03-03 14:38:14 +0100

    Created the cool Class Auto, Vroom Vroom

---
+++ src/Auto.java
@@ -0,0 +1,54 @@
+
+public class Auto {
+    private String make;
+    private String model;
+    private int year;
+    private double price;
+
+    // Constructor
+    public Auto(String make, String model, int year, double price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+
+    // Getter and Setter methods
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Car [make=" + make + ", model=" + model + ", year=" + year + ", price=" + price + "]";
+    }
+}
